aaf8ee5c2054dfc82db432982312cef8388e5188
MCLEANS <jackagwaya@gmail.com>
2024-10-17 10:55:24 +0300

    f(add-uart-driver): added usart support

--- mcu/hal_api/usart2.h
+++ mcu/hal_api/usart2.h
@@ -1,16 +1,26 @@
-#ifndef USART2_H_
-#define USART2_H_
+#ifndef _UART_H_
+#define _UART_H_
 
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
 
+#include "hal_api.h"
+
+
+#define UART_BUFFER_SIZE 256U
+/**
+ * @brief Flow control states
+ */
 typedef enum{
     UART2_FLOW_CONTROL_NONE,
     UART2_FLOW_CONTROL_HW,
 } uart2_flow_control_e;
 
-
+/**
+ * @brief UART operation errors
+ * 
+ */
 typedef enum {
     UART_OP_CONFIG_ERROR = -4,
     UART_OP_NO_DATA,
@@ -19,39 +29,82 @@ typedef enum {
     UART_OP_OK
 }uart_op_t;
 
-/**
- * @brief Initialize the UART port
- * 
- * @param baudrate 
- * @param flow_control 
- * @return int 
- */
-int usart2_init(uint32_t baudrate, unsigned int flow_control);
+typedef struct uart_cfg{
+    /* communication baud rate */
+    int baudrate;
+    /* UART port to use */
+    NRF_UARTE_Type* uart;
+    /* receive pin */
+    unsigned int rx_pin;
+    /* Transmit pin */
+    unsigned int tx_pin;
+    /* UART flow control */
+    unsigned int flow_control;
+}uart_cfg_t;
 
-/**
- * @brief Initialize RX and set the related callback
- * 
- * @return int operation error or the number of bytes received
- */
-int usart2_set_rx(void);
+/* Transmit buffer object */
+typedef struct {
+    uint8_t buffer[UART_BUFFER_SIZE];
+    int index;
+}tx_buffer_t;
 
-/**
- * @brief 
- * 
- * @param buf 
- * @param len 
- * @return uint32_t 
- */
-uint32_t usart2_tx(const void * buf, uint32_t len);
+typedef struct uart_t uart_t;
+struct uart_t {
+    /* UART configuration*/
+    uart_cfg_t cfg;
 
-/**
- * @brief 
- * 
- * @param data 
- * @param len 
- * @return int 
- */
-int usart2_rx(uint8_t* data, unsigned int len);
+    /* UART receive buffer*/
+    uint8_t rx_buffer[UART_BUFFER_SIZE];
+
+    /* UART transmit buffer */
+    tx_buffer_t tx_buffer;
+
+    /**
+     * @brief initialize the UART port
+     * @param self uart object
+     * @param cfg uart configuration
+     * @return int operaton status
+     */
+    int (*init)(struct uart_t *self, uart_cfg_t cfg);
+
+    /**
+     * @brief set the receive configuration of the UART
+     * @param self uart object
+     * @return int operation status
+     */
+    int (*set_rx)(struct uart_t *self);
+
+    /**
+     * @brief transmit data through UART port
+     * @param self uart object
+     * @param buf buffer containing data to transmit
+     * @param len length of the data to transmit
+     * @return error or number of bytes transmitted
+     * 
+     */
+    unsigned int (*tx)(struct uart_t *self, const void *buf, unsigned int len);
+
+    /**
+     * @brief Receive data though the UART port
+     * @param self uart object
+     * @param data buffer to store the received data
+     * @param len size of the receive buffer
+     * @return int error or number of bytes received
+     * 
+     */
+    int (*rx)(struct uart_t *self, uint8_t *data, unsigned int len);
+
+    /**
+     * @brief Clear the uart receive buffer
+     * @param self uart object
+     * @return int operation status
+     */
+    int (*clear_buffer)(struct uart_t *self);
+
+};
+
+/* Create a UART port */
+void uart_create(uart_t *self);
 
 
-#endif /* USART2_H_ */
+#endif // _UART_H_
--- mcu/nrf/nrf91/hal/usart2_dma.c
+++ mcu/nrf/nrf91/hal/usart2_dma.c
@@ -1,278 +1,227 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
-
 #include "board.h"
 #include "hal_api.h"
 #include "api.h"
 
-/** Declare buffer size (defined by max DMA transfert size) */
-#define BUFFER_SIZE 256u
-#include "doublebuffer.h"
+#define DEBUG_LOG_MODULE_NAME "USART M"
+#define DEBUG_LOG_MAX_LEVEL LVL_INFO
+#include "debug_log.h"
 
-/* Size of the RX Buffer */
-#define SYS_RX_BUFFER_SIZE 256
-/* Buffer to receive UART bytes */
-uint8_t usart2_m_rx_buffer[SYS_RX_BUFFER_SIZE];
+/* Private function declarations */
+static bool usart2_set_baud(uart_t *self);
+static int usart2_init_dma(uart_t *self);
+static int usart2_start_tx_lock(uart_t *self);
+static int usart2_set_rx(uart_t *self);
 
-/* Buffers used for transmission */
-static double_buffer_t usart2_m_tx_buffers;
+/* Method implementations */
+static int usart2_init(uart_t *self, uart_cfg_t cfg) {
 
-/** Set uarte baudrate */
-static bool usart2_set_baud(uint32_t baudrate);
-
-/** Initialize DMA part */
-static int usart2_init_dma(uint32_t baudrate);
-
-/** Called each time a transfer is ready or when a previous transfer was finished. This function must be called with interrupts
- *  disabled or from interrupt context
- */
-static int usart2_start_tx_lock(void);
-
-int usart2_init(uint32_t baudrate, unsigned int flow_control)
-{
-
-    nrf_gpio_cfg_default(BOARD_USART2_TX_PIN);
-    nrf_gpio_pin_set(BOARD_USART2_TX_PIN);
-    nrf_gpio_cfg_default(BOARD_USART2_RX_PIN);
-    nrf_gpio_pin_set(BOARD_USART2_RX_PIN);
-
-    /* Ensue UART is disables before configuration*/
-    NRF_UARTE1->ENABLE = UARTE_ENABLE_ENABLE_Disabled;
-
-    /* Set the pins to be used for UART TX and RX*/
-    NRF_UARTE1->PSEL.TXD = BOARD_USART2_TX_PIN;
-    NRF_UARTE1->PSEL.RXD = BOARD_USART2_RX_PIN;
-    NRF_UARTE1->TASKS_STOPTX = 1;
-    NRF_UARTE1->TASKS_STOPRX = 1;
-
-    /* Set the UART communication baudrate */
-    if(!usart2_set_baud(baudrate)){
+    if(self == NULL){
         return UART_OP_CONFIG_ERROR;
     }
 
-    /* Initialize DMA p0rt*/
-    if(usart2_init_dma(baudrate) != UART_OP_OK){
+    self->cfg = cfg;
+
+    nrf_gpio_cfg_default(self->cfg.tx_pin);
+    nrf_gpio_pin_set(self->cfg.tx_pin);
+    // nrf_gpio_cfg_default(self->cfg.rx_pin);
+    // nrf_gpio_pin_set(self->cfg.rx_pin);
+
+    self->cfg.uart->ENABLE = UARTE_ENABLE_ENABLE_Disabled;
+    self->cfg.uart->PSEL.TXD = self->cfg.tx_pin;
+    self->cfg.uart->PSEL.RXD = self->cfg.rx_pin;
+    self->cfg.uart->TASKS_STOPTX = 1;
+    self->cfg.uart->TASKS_STOPRX = 1;
+
+    if (!usart2_set_baud(self)) {
         return UART_OP_CONFIG_ERROR;
     }
 
-    /* Configure TX pin and Enable UART*/
-    nrf_gpio_cfg(BOARD_USART2_TX_PIN,
-                         NRF_GPIO_PIN_DIR_OUTPUT,
-                         NRF_GPIO_PIN_INPUT_CONNECT,
-                         NRF_GPIO_PIN_NOPULL,
-                         NRF_GPIO_PIN_S0S1,
-                         NRF_GPIO_PIN_NOSENSE);
-
-    NRF_UARTE1->ENABLE = UARTE_ENABLE_ENABLE_Enabled;
-
-    /* Set rx parameters for the UART */
-    if(usart2_set_rx() != UART_OP_OK){
+    if (usart2_init_dma(self) != UART_OP_OK) {
         return UART_OP_CONFIG_ERROR;
     }
+
+    nrf_gpio_cfg(self->cfg.tx_pin,
+                 NRF_GPIO_PIN_DIR_OUTPUT,
+                 NRF_GPIO_PIN_INPUT_CONNECT,
+                 NRF_GPIO_PIN_NOPULL,
+                 NRF_GPIO_PIN_S0S1,
+                 NRF_GPIO_PIN_NOSENSE);
+
+    
+
+    self->cfg.uart->CONFIG = 0;
+
+    if (usart2_set_rx(self) != UART_OP_OK) {
+        return UART_OP_CONFIG_ERROR;
+    }
+
+    self->cfg.uart->ENABLE = UARTE_ENABLE_ENABLE_Enabled;
 
     return UART_OP_OK;
 }
 
-int usart2_set_rx(void){
+static int usart2_set_rx(uart_t *self) {
 
     Sys_enterCriticalSection();
 
-    /* Enable RX input */
-    nrf_gpio_cfg(BOARD_USART2_RX_PIN,
-                     NRF_GPIO_PIN_DIR_INPUT,
-                     NRF_GPIO_PIN_INPUT_CONNECT,
-                     NRF_GPIO_PIN_NOPULL,
-                     NRF_GPIO_PIN_S0S1,
-                     NRF_GPIO_PIN_SENSE_LOW);
-    
-    /* Clear all active events */
-    NRF_UARTE1->EVENTS_RXDRDY = 0;
-    NRF_UARTE1->EVENTS_ENDRX = 0;
-    NRF_UARTE1->EVENTS_RXSTARTED = 0;
+    nrf_gpio_cfg(self->cfg.rx_pin,
+                 NRF_GPIO_PIN_DIR_INPUT,
+                 NRF_GPIO_PIN_INPUT_CONNECT,
+                 NRF_GPIO_PIN_PULLUP,
+                 NRF_GPIO_PIN_S0S1,
+                 NRF_GPIO_PIN_SENSE_LOW);
 
-    // Prepare RX buffer
-    NRF_UARTE1->RXD.PTR = (uint32_t) usart2_m_rx_buffer;
-    NRF_UARTE1->RXD.MAXCNT = SYS_RX_BUFFER_SIZE;
+    self->cfg.uart->EVENTS_RXDRDY = 0;
+    self->cfg.uart->EVENTS_ENDRX = 0;
+    self->cfg.uart->EVENTS_RXSTARTED = 0;
 
-    /*Start the Uart again */
-    NRF_UARTE1->EVENTS_RXSTARTED = 0;
-    NRF_UARTE1->TASKS_STARTRX = 1;
-
-    // Confirm that the reception has been started again */
-    while(NRF_UARTE1->EVENTS_RXSTARTED != 1){}
-
+    self->cfg.uart->RXD.PTR = (uint32_t) self->rx_buffer;
+    self->cfg.uart->RXD.MAXCNT = UART_BUFFER_SIZE;
 
     Sys_exitCriticalSection();
 
     return UART_OP_OK;
 }
 
-int usart2_rx(uint8_t* data, unsigned int len){
+static int usart2_clear_buffer(uart_t *self) {
 
     Sys_enterCriticalSection();
 
-    /* Stop data reception */
-    NRF_UARTE1->TASKS_STOPRX = 1;
+    self->cfg.uart->TASKS_STOPRX = 1;
 
-    /* Wait for the reception to stop*/
-    while(NRF_UARTE1->EVENTS_ENDRX != 1){}
+    while (self->cfg.uart->EVENTS_RXTO != 1) {}
 
-    /**
-     * For some reason the RXD.AMOUNT Register still returns zero at this point
-     * @todo Find out why and use it to get the number of bytes received.
-     */
+    memset(self->rx_buffer, 0, sizeof(self->rx_buffer));
 
-    int bytes = (int) strlen((const char*) usart2_m_rx_buffer);
+    self->cfg.uart->RXD.PTR = (uint32_t) self->rx_buffer;
+    self->cfg.uart->RXD.MAXCNT = UART_BUFFER_SIZE;
 
-    if(bytes == 0 ){
+    self->cfg.uart->TASKS_STARTRX = 1;
+
+    while (self->cfg.uart->EVENTS_RXSTARTED != 1) {}
+
+    Sys_exitCriticalSection();
+
+    return UART_OP_OK;
+}
+
+static unsigned int usart2_tx(uart_t *self, const void *buffer, unsigned int length) {
+
+    Sys_enterCriticalSection();
+
+    if (usart2_clear_buffer(self) != UART_OP_OK) {
+        Sys_exitCriticalSection();
+        return 0;
+    }
+
+    if(length > UART_BUFFER_SIZE){
+        Sys_exitCriticalSection();
+        return 0;
+    }
+    memcpy(self->tx_buffer.buffer,
+           buffer,
+           length);
+
+    self->tx_buffer.index = length;
+
+    int ret = usart2_start_tx_lock(self);
+
+    Sys_exitCriticalSection();
+
+    return ret;
+}
+
+static int usart2_rx(uart_t *self, uint8_t *data, unsigned int len) {
+
+    Sys_enterCriticalSection();
+
+    self->cfg.uart->TASKS_STOPRX = 1;
+
+    while (self->cfg.uart->EVENTS_ENDRX != 1) {}
+
+    int bytes = (int) strlen((const char*) self->rx_buffer);
+
+    if (bytes == 0) {
         Sys_exitCriticalSection();
         return UART_OP_NO_DATA;
     }
 
-    /* Copy the received bytes from the rx_buffer to user buffer */
-    memcpy(data, usart2_m_rx_buffer, bytes);
+    memcpy(data, self->rx_buffer, bytes);
 
-    /*Start the Uart again */
-    NRF_UARTE1->EVENTS_RXSTARTED = 0;
-    NRF_UARTE1->TASKS_STARTRX = 1;
+    self->cfg.uart->TASKS_STARTRX = 1;
 
-    /* Confirm that the reception has been started again */
-    while(NRF_UARTE1->EVENTS_RXSTARTED != 1){}
+    while (self->cfg.uart->EVENTS_RXSTARTED != 1) {}
 
     Sys_exitCriticalSection();
 
     return bytes;
-
 }
 
-int usart2_clear_buffer(void){
+static bool usart2_set_baud(uart_t *self) {
 
-    Sys_enterCriticalSection();
+    switch (self->cfg.baudrate) {
+        case 115200:
+            self->cfg.uart->BAUDRATE = (uint32_t) UARTE_BAUDRATE_BAUDRATE_Baud115200;
+            break;
+        case 125000:
+            self->cfg.uart->BAUDRATE = (uint32_t)(0x02000000UL);
+            break;
+        case 250000:
+            self->cfg.uart->BAUDRATE = (uint32_t) UARTE_BAUDRATE_BAUDRATE_Baud250000;
+            break;
+        case 460800:
+            self->cfg.uart->BAUDRATE = (uint32_t) UARTE_BAUDRATE_BAUDRATE_Baud460800;
+            break;
+        case 1000000:
+            self->cfg.uart->BAUDRATE = (uint32_t) UARTE_BAUDRATE_BAUDRATE_Baud1M;
+            break;
+        case 2400:
+            self->cfg.uart->BAUDRATE = (uint32_t) UARTE_BAUDRATE_BAUDRATE_Baud2400;
+            break; 
+        case 9600:
+            self->cfg.uart->BAUDRATE = (uint32_t) UARTE_BAUDRATE_BAUDRATE_Baud9600;
+            break; 
+        default:
+            return false;
+    };
 
-    /* Stop RX */
-    NRF_UARTE1->TASKS_STOPRX = 1;
-    while(NRF_UARTE1->EVENTS_RXTO != 1){}
-
-    /* Clear buffer*/
-    memset(usart2_m_rx_buffer, 0, sizeof(usart2_m_rx_buffer));
-
-    /* Start RX*/
-    NRF_UARTE1->EVENTS_RXSTARTED = 0;
-    NRF_UARTE1->TASKS_STARTRX = 1;
-
-    /* Confirm that the reception has been started again */
-    while(NRF_UARTE1->EVENTS_RXSTARTED != 1){}
-
-    Sys_exitCriticalSection();
-
-    return UART_OP_OK;
+    return true;
 }
 
-uint32_t usart2_tx(const void * buffer, uint32_t length)
-{
-    Sys_enterCriticalSection();
+static int usart2_init_dma(uart_t *self) {
 
-    if(usart2_clear_buffer() != UART_OP_OK){
-        return 0;
-    }
+    self->cfg.uart->INTENCLR = 0xffffffffUL;
 
-    // Check if there is enough room
-    if (BUFFER_SIZE - DoubleBuffer_getIndex(usart2_m_tx_buffers) < length)
-    {
-        Sys_exitCriticalSection();
-        return 0;
-    }
-
-    // Copy data to current buffer
-    memcpy(DoubleBuffer_getActive(usart2_m_tx_buffers) +
-           DoubleBuffer_getIndex(usart2_m_tx_buffers),
-           buffer,
-           length);
-
-    DoubleBuffer_incrIndex(usart2_m_tx_buffers, length);
-
-    int ret =  usart2_start_tx_lock();
-
-    Sys_exitCriticalSection();
-
-    return ret;
-}
-
-static int usart2_start_tx_lock(void)
-{
-
-    if (DoubleBuffer_getIndex(usart2_m_tx_buffers) == 0)
-    {
-        // Nothing to send
-        return -1;
-    }
-
-    // Start DMA
-    NRF_UARTE1->TXD.PTR = (uint32_t) DoubleBuffer_getActive(usart2_m_tx_buffers);
-    NRF_UARTE1->TXD.MAXCNT = DoubleBuffer_getIndex(usart2_m_tx_buffers);
-
-    NRF_UARTE1->EVENTS_ENDTX = 0;
-    NRF_UARTE1->TASKS_STARTTX = 1;
-
-    /* Wait for the transmission to complete */
-    while(NRF_UARTE1->EVENTS_ENDTX == 0){}
-
-    // Swipe buffers (it automatically reset writing index)
-    DoubleBuffer_swipe(usart2_m_tx_buffers);
-
-    /** Return number of bytes transmitted  */
-    return NRF_UARTE1->TXD.AMOUNT;
-}
-
-
-static bool usart2_set_baud(uint32_t baudrate)
-{
-    bool ret = true;
-    switch (baudrate)
-    {
-    case 115200:
-        NRF_UARTE1->BAUDRATE = (uint32_t)UARTE_BAUDRATE_BAUDRATE_Baud115200;
-        break;
-    case 125000:
-        // This value is not from official nrf9160_bitfields.h
-        NRF_UARTE1->BAUDRATE = (uint32_t)(0x02000000UL);
-        break;
-    case 250000:
-        NRF_UARTE1->BAUDRATE = (uint32_t)UARTE_BAUDRATE_BAUDRATE_Baud250000;
-        break;
-    case 460800:
-        NRF_UARTE1->BAUDRATE = (uint32_t)UARTE_BAUDRATE_BAUDRATE_Baud460800;
-        break;
-    case 1000000:
-        NRF_UARTE1->BAUDRATE = (uint32_t)UARTE_BAUDRATE_BAUDRATE_Baud1M;
-        break;
-    default:
-        // Intended baudrate is not in the list, default baudrate from chip will be used
-        ret = false;
-        break;
-    }
-
-    return ret;
-}
-
-static int usart2_init_dma(uint32_t baudrate)
-{
-
-    NRF_UARTE1->INTENCLR = 0xffffffffUL;
-    NRF_UARTE1->INTENSET =
+    self->cfg.uart->INTENSET =
         (UARTE_INTEN_ENDTX_Enabled << UARTE_INTEN_ENDTX_Pos) |
         (UARTE_INTEN_ERROR_Enabled << UARTE_INTEN_ERROR_Pos);
 
-    NRF_UARTE0->SHORTS =
-        UARTE_SHORTS_ENDRX_STARTRX_Enabled << UARTE_SHORTS_ENDRX_STARTRX_Pos;
- 
-    /* Configure TX part */
-    DoubleBuffer_init(usart2_m_tx_buffers);
-
-    if(DoubleBuffer_getIndex(usart2_m_tx_buffers) != 0){
-        return UART_OP_CONFIG_ERROR;
-    }
-
     return UART_OP_OK;
+}
+
+static int usart2_start_tx_lock(uart_t *self) {
+
+    self->cfg.uart->TXD.PTR = (uint32_t) self->tx_buffer.buffer;
+    self->cfg.uart->TXD.MAXCNT = self->tx_buffer.index;
+
+    self->cfg.uart->EVENTS_ENDTX = 0;
+    self->cfg.uart->TASKS_STARTTX = 1;
+
+    while (self->cfg.uart->EVENTS_ENDTX == 0) {}
+
+    return self->cfg.uart->TXD.AMOUNT;
+}
+
+/* Function to initialize method pointers */
+void uart_create(uart_t *self) {
+
+    self->init = usart2_init;
+    self->set_rx = usart2_set_rx;
+    self->tx = usart2_tx;
+    self->rx = usart2_rx;
+    self->clear_buffer = usart2_clear_buffer;
+
 }
